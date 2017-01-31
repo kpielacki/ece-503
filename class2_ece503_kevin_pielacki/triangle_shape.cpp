aba00d62c65ec57aeac50c5b1a1892033e1f832a
kevin <kpielacki@gmail.com>
2017-01-30 23:06:30 -0500

    Comments for cpp file

--- class2_ece503_kevin_pielacki/triangle_shape.cpp
+++ class2_ece503_kevin_pielacki/triangle_shape.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 
 int main() {
+	// Store user input for number of lines to print.
 	int num_lines;
 
 	cout << "Enter the number of lines to print: ";
 	cin >> num_lines;
 
+	// Iterate through lines
 	for (int i = 0; i < num_lines; i++ ) {
+		// Iterate through star print.
 		for ( int j = 0; j <= i; j++ ) {
 			cout << "*";
 		}
+		// After star print begin new line.
 		cout << endl;
 	}
 
