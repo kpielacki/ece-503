afefe7c3810a81964a90a547a662eaaa2505107c
Kevin <kpielacki@gmail.com>
2017-02-22 23:06:25 -0500

    Finished recursive function square print (part 1)

--- hw5_ece503_kevin_pielacki/hw5_kevin_pielacki_square_print.cpp
+++ hw5_ece503_kevin_pielacki/hw5_kevin_pielacki_square_print.cpp
@@ -3,30 +3,57 @@
 using namespace std;
 
 
-vector < int > get_counting_nums(int n) {
-    vector < int > counting;
-    for (int i = 0; i < n; i++) {
-        counting.push_back(i);
-    }
-    return counting;
+void recurse_square_print(int n) {
+	// Takes int value and prints square of odd numbers descending
+	// followed by square of even number ascending with comma
+	// seperated values.
+
+	if (n >= 0) {
+	    if (n % 2 == 1) {
+	    	// Set pointer value to next odd number desc.
+	    	int new_n = n - 2;
+	    	int next_even_n = n - 1;
+
+	    	// Check to add comma after square print.
+	    	// During the positive print the comma will begin
+	    	// the console output instead of end it.
+	    	if (n == 1) {
+	    		cout << n * n;
+	    	}
+	    	else {
+		    	cout << n * n << ", ";
+	    	}
+		    recurse_square_print(new_n);
+		    // Only print postive n.
+		    if (next_even_n > 0) {
+		    	cout << ", " << next_even_n * next_even_n;
+		    }
+	    } else {
+	    	// Move to next odd number.
+	    	int new_n = n - 1;
+	    	// Odd number prints.
+		    recurse_square_print(new_n);
+		    // Need to print even number from first function call.
+		    cout << ", " << n * n << endl;
+	    }
+	}
+	return;
 }
 
 
 int main() {
-    int n = -1;
+	// Gets user input to call recursive square print.
+	int n = -1;
 
-    
-    while (n < 0) {
+	// Exclude non-positive integers.
+    while (n <= 0) {
         cout << "Enter a positive integer: ";
         cin >> n;
-
-
-        if (n < 0) {
+        if (n <= 0) {
+        	// Tell user integers must be positive.
             cout << "Integer must be positive." << endl;
         }
     }
-
-    get_counting_nums(n);
-
+    recurse_square_print(n);
     return 0;
 }
