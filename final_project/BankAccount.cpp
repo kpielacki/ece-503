d39c332a3f5283d87c7e99ce70fda1fc6dd834c7
Kevin <kpielacki@gmail.com>
2017-04-09 21:51:32 -0400

    Better print formatting and txt var storage

--- final_project/BankAccount.cpp
+++ final_project/BankAccount.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include "Account.h"
 #include "BankAccount.h"
 
@@ -31,7 +32,7 @@ void BankAccount::withdraw(double amount) {
             // Open file to append new transcation.
             std::ofstream transaction_history_file;
             transaction_history_file.open(transaction_history_filename.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
-            transaction_history_file << "Withdrawal\t$" << amount << "\tDATE\t" << new_balance << "\n";
+            transaction_history_file << "Withdrawal " << amount << " DATE " << new_balance << "\n";
 
             // Adjust cash balance to reflect transaction.
             BankAccount::set_cash_balance(new_balance);
@@ -49,7 +50,7 @@ void BankAccount::deposit(double amount) {
         // Open file to append new transcation.
         std::ofstream transaction_history_file;
         transaction_history_file.open(transaction_history_filename.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
-        transaction_history_file << "Deposit\t$" << amount << "\tDATE\t" << new_balance << "\n";
+        transaction_history_file << "Deposit " << amount << " DATE " << new_balance << "\n";
 
         // Adjust cash balance to reflect transaction.
         BankAccount::set_cash_balance(new_balance);
@@ -66,14 +67,18 @@ void BankAccount::view_account_balance() {
 
 void BankAccount::print_transaction_history() {
     // Print transaction history headers.
-    std::string line;
+    std::string line, event, date;
+    double amount, balance;
+
     std::ifstream transaction_history_file;
     transaction_history_file.open(transaction_history_filename.c_str());
 
     // Header and transaction history print.
-    std::cout << "Event\tAmount\tDate\tBalance" << std::endl;
-    while (std::getline(transaction_history_file, line)) {
-        std::cout << line << std::endl;
+    printf("%-12s%-16s%-12s%-16s\n", "Event", "Amount", "Date", "Balance");
+    while (getline(transaction_history_file, line)) {
+	std::istringstream ss(line);
+	ss >> event >> amount >> date >> balance;
+        printf("%-12s$%-15.2f%-12s$%-15.2f\n", event.c_str(), amount, date.c_str(), balance);
     }
     transaction_history_file.close();
 }
